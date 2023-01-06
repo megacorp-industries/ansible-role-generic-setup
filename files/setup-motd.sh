3e995bd2fc9e2bea559f52db7a5aa70c4ee6f991
megacorp-industries <121791047+megacorp-industries@users.noreply.github.com>
2023-01-06 14:00:11 +0000

    Create setup-motd.sh

---
+++ files/setup-motd.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+chmod 775 /usr/bin/update_motd.sh  # Set permissions on MOTD script
+sed -i 's;session    optional     pam_motd.so  motd=/etd/motd;#session    optional     pam_motd.so  motd=/etc/motd;g' /etc/pam.d/sshd  # Set PAM to execute script upon each new session
+sed -i 's;session    optional     pam_motd.so noupdate;#session    optional     pam_motd.so noupdate;g' /etc/pam.d/sshd  # Prevent MOTD from showing twice
+sed -i 's;session    optional     pam_motd.so  motd=/run/motd.dynamic;#session    optional     pam_motd.so  motd=/run/motd.dynamic;g' /etc/pam.d/sshd  # Prevent MOTD from showing twice
+printf 'session   optional   pam_exec.so   stdout /usr/bin/update_motd.sh\nsession   optional   pam_motd.so   motd=/etc/motd' | sudo tee -a /etc/pam.d/sshd  # Set PAM to execute script upon each new session
