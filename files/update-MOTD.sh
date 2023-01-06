fa3e797af8cdf8af280d12859c27ca562e018faa
megacorp-industries <121791047+megacorp-industries@users.noreply.github.com>
2023-01-06 14:08:20 +0000

    Update update-MOTD.sh

--- files/update-MOTD.sh
+++ files/update-MOTD.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-chmod 775 /usr/bin/update_motd.sh  # Set permissions on MOTD script
+chmod 775 /usr/bin/MOTD.sh  # Set permissions on MOTD script
 sed -i 's;session    optional     pam_motd.so  motd=/etd/motd;#session    optional     pam_motd.so  motd=/etc/motd;g' /etc/pam.d/sshd  # Set PAM to execute script upon each new session
 sed -i 's;session    optional     pam_motd.so noupdate;#session    optional     pam_motd.so noupdate;g' /etc/pam.d/sshd  # Prevent MOTD from showing twice
 sed -i 's;session    optional     pam_motd.so  motd=/run/motd.dynamic;#session    optional     pam_motd.so  motd=/run/motd.dynamic;g' /etc/pam.d/sshd  # Prevent MOTD from showing twice
